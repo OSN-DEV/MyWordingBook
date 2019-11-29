acf24407538881f9a5735e594c638586c4ee4082
OSN-DEV <osndev1973@gmail.com>
2019-11-29 18:16:29 +0900

    create skeleton and add restore window function

---
+++ Data/AppRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib.Data;
+
+namespace MyWordingBook.Data {
+    public class AppRepository : AppDataBase<AppRepository> {
+        #region Declaration   
+        public class Location {
+            public double X { set; get; } = -1;
+            public double Y { set; get; } = -1;
+        }
+        public class Rect {
+            public double W { set; get; } = -1;
+            public double H { set; get; } = -1;
+        }
+        public Location Pos { set; get; } = new Location();
+        public Rect Size { set; get; } = new Rect();
+        private static string _settingFile;
+        #endregion
+
+        #region Public Method
+        public static AppRepository Init(string file) {
+            _settingFile = file;
+            GetInstanceBase(file);
+            if (!System.IO.File.Exists(file)) {
+                _instance.Save();
+            }
+            return _instance;
+        }
+
+        /// <summary>
+        /// get instance
+        /// </summary>
+        /// <returns></returns>
+        public static AppRepository GetInstance() {
+            return GetInstanceBase();
+        }
+
+        /// <summary>
+        /// save settings
+        /// </summary>
+        public void Save() {
+            GetInstanceBase().SaveToXml(_settingFile);
+        }
+        #endregion
+    }
+}
---
+++ MyWordingBookMain.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyLib.Util;
+using MyWordingBook.Data;
+using MyWordingBook.Util;
+
+namespace MyWordingBook {
+    /// <summary>
+    /// main window
+    /// </summary>
+    public partial class MainWindow : Window {
+        #region Declaration
+        private AppRepository _settings;
+        #endregion
+
+
+        #region Constructor
+        public MainWindow() {
+            InitializeComponent();
+            this.Initialize();
+        }
+#endregion
+
+
+        #region Event   
+        /// <summary>
+        /// window keydown
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_KeyDown(object sender, KeyEventArgs e) {
+            switch(e.Key) {
+                // Ctrl + W : Close App
+                case Key.W:
+                    if (Common.IsModifierPressed(ModifierKeys.Control)) {
+                        e.Handled = true;
+                        this.SaveWindowInfo();
+                        this.Close();
+                    }
+                    break;
+            }
+        }
+        #endregion
+
+        #region Private Method
+        /// <summary>
+        /// initialize window
+        /// </summary>
+        private void Initialize() {
+            this._settings = AppRepository.Init(Constant.SettingFile);
+
+            // restore window position
+            double pos = Common.GetWindowPosition(this._settings.Pos.X, this._settings.Size.W, SystemParameters.VirtualScreenWidth);
+            if (0<= pos) {
+                this.Left = pos;
+            }
+            pos = Common.GetWindowPosition(this._settings.Pos.Y, this._settings.Size.W, SystemParameters.VirtualScreenWidth);
+            if (0 <= pos) {
+                this.Top = pos;
+            }
+
+            // restore window size
+            this.Width = Common.GetWindowSize(this.Width, this._settings.Size.W, SystemParameters.WorkArea.Width);
+            this.Height = Common.GetWindowSize(this.Height, this._settings.Size.H, SystemParameters.WorkArea.Height);
+        }
+
+
+        /// <summary>
+        /// save window size and position
+        /// </summary>
+        private void SaveWindowInfo() {
+            this._settings.Pos.X = this.Left;
+            this._settings.Pos.Y = this.Top;
+            this._settings.Size.W = this.Width;
+            this._settings.Size.H = this.Height;
+            this._settings.Save();
+        }
+        #endregion
+    }
+}
